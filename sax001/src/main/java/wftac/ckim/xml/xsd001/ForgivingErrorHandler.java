b5cc21e1d65cdbc51d83d5d26f9f4c9a00fd3762
kimcharli <kimcharli@650e5670-e1cd-11dd-b00f-9fabb4b2ff13>
2009-08-12 07:11:19 +0000

    added xsd part

---
+++ sax001/src/main/java/wftac/ckim/xml/xsd001/ForgivingErrorHandler.java
@@ -0,0 +1,21 @@
+package wftac.ckim.xml.xsd001;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class ForgivingErrorHandler implements ErrorHandler {
+
+    public void warning(SAXParseException ex) {
+        System.err.println(ex.getMessage());
+    }
+
+    public void error(SAXParseException ex) {
+        System.err.println(ex.getMessage());
+    }
+
+    public void fatalError(SAXParseException ex) throws SAXException {
+        throw ex;
+    }
+
+}
